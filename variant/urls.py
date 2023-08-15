2966a34abbccb2ac1d4aa8801378fab7287dfe2a
nguyentrinhtrungduong <nguyentrinhtrungduong@gmail.com>
2023-08-15 10:24:41 +0200

    Style 3d primary sequence

--- variant/urls.py
+++ variant/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import get_genebass_tables
+from .views import get_genebass_tables, get_variant_vep_scores_and_plot
 
 app_name = 'variant'
 
 urlpatterns = [
     path('genebass-variants/', get_genebass_tables, name='get-genebass-tables'),
+    path('plot-variants/', get_variant_vep_scores_and_plot, name='get-plots'),
     ]
--- variant/views.py
+++ variant/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.shortcuts import render
+import numpy as np
+import json
 
 from .models import (
     GenebassVariant,
@@ -8,8 +11,90 @@ from .models import (
 )
 
 
+def get_variant_vep_scores_and_plot(request):
+    """
+        Get the vep scores for a specific variant
+    """
+    variant_marker = request.GET.get("variant_marker")
+
+    # Get variant
+    try:
+        variant = Variant.objects.filter(VariantMarker=variant_marker).first()
+    except Variant.DoesNotExist:
+        variant = None
+
+    # Get gene
+    if variant:
+        gene = variant.Gene_ID
+    else:
+        gene = None
+
+    # Get transcript
+    transcript_ids = VepVariant.objects.filter(Variant_marker=variant_marker).values_list('Transcript_ID', flat=True)
+
+    list_vep_scores = VepVariant.objects.filter(Variant_marker=variant_marker).values(
+                    "BayesDel_addAF_rankscore",
+                    "BayesDel_noAF_rankscore",
+                    "CADD_raw_rankscore",
+                    "ClinPred_rankscore",
+                    "DANN_rankscore",
+                    "DEOGEN2_rankscore",
+                    "Eigen_PC_raw_coding_rankscore",
+                    "Eigen_raw_coding_rankscore",
+                    "FATHMM_converted_rankscore",
+                    "GERP_RS_rankscore",
+                    "GM12878_fitCons_rankscore",
+                    "GenoCanyon_rankscore",
+                    "H1_hESC_fitCons_rankscore",
+                    "HUVEC_fitCons_rankscore",
+                    "LIST_S2_rankscore",
+                    "LRT_converted_rankscore",
+                    "M_CAP_rankscore",
+                    "MPC_rankscore",
+                    "MVP_rankscore",
+                    "MetaLR_rankscore",
+                    "MetaRNN_rankscore",
+                    "MetaSVM_rankscore",
+                    "MutPred_rankscore",
+                    "MutationAssessor_rankscore",
+                    "MutationTaster_converted_rankscore",
+                    "PROVEAN_converted_rankscore",
+                    "Polyphen2_HDIV_rankscore",
+                    "Polyphen2_HVAR_rankscore",
+                    "PrimateAI_rankscore",
+                    "REVEL_rankscore",
+                    "SIFT4G_converted_rankscore",
+                    "SIFT_converted_rankscore",
+                    "SiPhy_29way_logOdds_rankscore",
+                    "VEST4_rankscore",
+                    "bStatistic_converted_rankscore",
+                    "Fathmm_MKL_coding_rankscore",
+                    "Fathmm_XF_coding_rankscore",
+                    "Integrated_fitCons_rankscore",
+                    "PhastCons30way_mammalian_rankscore",
+                    "PhyloP30way_mammalian_rankscore",
+                    "LINSIGHT_rankscore",
+    )
+
+    vep_scores_data = {
+        "x": list(list_vep_scores.keys()),
+        "y": [list(scores) for scores in list_vep_scores.values()],
+        "type": "box",
+        "name": "Variant Scores",
+    }
+
+    context = {
+        "vep_scores_data": json.dumps(vep_scores_data),
+    }
+
+    return render(request, "variant_plot.html", context)
+
+
+
+
 def get_genebass_tables(request):
-    """Get the genebass tables for a variant.
+    """
+        Get the genebass tables for a variant.
     """
     variant_marker = request.GET.get("variant_marker")
     # Get variant
